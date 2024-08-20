363bea2a0bfe898dffb66694b019cceeea705e44
AndreyTu <139591226+AndreyTu@users.noreply.github.com>
2024-08-20 15:00:13 +0300

    Puppeter_2

--- 7.5/puppeteer-2/features/step_definitions/search.steps.js
+++ 7.5/puppeteer-2/features/step_definitions/search.steps.js
@@ -2,11 +2,19 @@ const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
 const { Given, When, Then, Before, After } = require("cucumber");
-const { putText, getText } = require("../../lib/commands.js");
+const { getText, clickElement } = require("../../lib/commands.js");
+
+let browser;
+let page;
+let place;
 
 Before(async function () {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
-  const page = await browser.newPage();
+  browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--start-maximized"],
+  });
+  page = await browser.newPage();
   this.browser = browser;
   this.page = page;
 });
@@ -17,18 +25,81 @@ After(async function () {
   }
 });
 
-Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`https://netology.ru${string}`, {
-    setTimeout: 20000,
+Given("пользователь на странице {string}", async function (url) {
+  try {
+    await this.page.goto(url, { setTimeout: 50000 });
+  } catch (error) {
+    throw new Error(`Failed to navigate to ${url} with error: ${error}`);
+  }
+});
+
+When("переходит на расписание на завтра", async function () {
+  return await clickElement(this.page, "nav > a:nth-child(2)");
+});
+
+When("выбирает время сеанса на Ведьмак на 20-00", async function () {
+  return await clickElement(this.page, "body > main > section:nth-child(1) > div.movie-seances__hall > ul > li:nth-child(2) > a");
+});
+
+When("выбирает место в зале кинотеатра 6 ряд 6 место", async function () {
+  await this.page.waitForSelector("div.buying-scheme");
+  place = ".buying-scheme__wrapper > :nth-child(6) > :nth-child(6)";
+  await clickElement(this.page, place);
+  await clickElement(this.page, "button.acceptin-button");
+});
+
+Then("получает результат брони до покупки", async function () {
+  await this.page.waitForSelector("h2");
+  let filmTitle = await getText(this.page, `body > main > section > div > p:nth-child(1) > span`);
+  expect(filmTitle).equal("Ведьмак");
+  let placeNumber = await getText(this.page, `body > main > section > div > p:nth-child(2) > span`);
+  expect(placeNumber).equal("6/6");
+  let price = await getText(this.page, `body > main > section > div > p:nth-child(6) > span`);
+  expect(price).equal("100");
+});
+
+When("переходит на расписание на послезавтра", async function () {
+  return await clickElement(this.page, "nav > a:nth-child(3)");
+});
+
+When("выбирает время сеанса на Унесенные ветром на 17-00", async function () {
+  return await clickElement(this.page, "body > main > section:nth-child(3) > div:nth-child(2) > ul > li > a");
+});
+
+When("выбирает места в зале кинотеатра 5 ряд 4 место",async function () {
+  await page.waitForSelector("div.buying-scheme");
+  const place = ".buying-scheme__wrapper > :nth-child(5) > :nth-child(4)";
+  await clickElement(page, place);
+  await clickElement(page, "button.acceptin-button");
+  }
+);
+
+Then("получает результат брони до покупки", async function () {
+  await page.waitForSelector("h2");
+  let filmTitle = await getText(page, `body > main > section > div > p:nth-child(1) > span`);
+  expect(filmTitle).equal("Унесенные ветром.");
+  let placeNumber = await getText(page, `body > main > section > div > p:nth-child(2) > span`);
+  expect(placeNumber).equal("5/4");
+  let price = await getText(page, `body > main > section > div > p:nth-child(6) > span`);
+  expect(price).equal("150");
+});
+
+
+When("выбирает время сеанса на Микки Маус на 11-00", async function () {
+    return await clickElement(this.page, "body > main > section:nth-child(2) > div.movie-seances__hall > ul > li:nth-child(1) > a");
+  }
+);
+
+When("выбирает место в зале кинотеатра 4 ряд 3 место", async function () {
+  await page.waitForSelector("div.buying-scheme");
+  const place = ".buying-scheme__wrapper > :nth-child(4) > :nth-child(3)";
+  await clickElement(page, place);
+  await clickElement(page, "button.acceptin-button");
+});
+
+Then("место занято и получает результат", async function () {
+  const stateOfButton = await page.$eval('button', (button) => {
+    return button.disabled;
   });
-});
-
-When("user search by {string}", async function (string) {
-  return await putText(this.page, "input", string);
-});
-
-Then("user sees the course suggested {string}", async function (string) {
-  const actual = await getText(this.page, "a[data-name]");
-  const expected = await string;
-  expect(actual).contains(expected);
+  expect(stateOfButton).equal(true);
 });
